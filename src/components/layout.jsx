cbb468d1058571e7d81a154b9478d6f370fbc64f
Armin Pfurtscheller <armin.pfurtscheller@cropster.org>
2019-01-24 15:57:03 +0100

    fix

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -58,7 +58,9 @@ export const SubPage = ({ html }) => {
 		props: {
 			children
 		}
-	} = markdownToJSX(html);
+	} = markdownToJSX(html, {
+		forceBlock: true
+	});
 
 	const items = children
 		.concat([
